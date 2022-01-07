8d7a1794e49b7fa1780a4707ed01c580e09f90ee
Neha Yadav <nehayadav903@gmail.com>
2022-01-07 14:58:47 -0500

    Fix the string error

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,18 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-Store.create(name: Burnaby,
-             annual_revenue: true,
+burnaby = Store.create(name: "Burnaby",
+             annual_revenue: 300000,
              mens_apparel: true,
              womens_apparel:true
               )
-Store.create(name: Richmond,
-             annual_revenue: true,
+richmond = Store.create(name: "Richmond",
+             annual_revenue: 1260000,
              mens_apparel: false,
              womens_apparel:true
             )
-Store.create(name: Gastown,
-             annual_revenue: true,
+gastown = Store.create(name: "Gastown",
+             annual_revenue: 1900000,
              mens_apparel: true,
              womens_apparel:false
             )     
